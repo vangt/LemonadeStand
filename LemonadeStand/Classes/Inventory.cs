cd29f8537b2407198edd282586f7a862a4f63f3e
vangt <vangt86@gmail.com>
2017-08-08 15:48:50 -0500

    Created Inventory Class and added member variables and method calls to increase or decrease variables.

---
+++ LemonadeStand/Classes/Inventory.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand.Classes
+{
+    public class Inventory
+    {
+        public double lemon;
+        public double sugar;
+        public double ice;
+
+        public Inventory()
+        {
+
+        }
+
+        public void AddLemon(int number)
+        {
+            lemon += number;
+        }
+
+        public void AddSugar(int number)
+        {
+            sugar += number;
+        }
+
+        public void AddIce(int number)
+        {
+            ice += number;
+        }
+
+        public void RemoveLemon(int number)
+        {
+            lemon -= number;
+
+            if(lemon < 0)
+            {
+                lemon = 0;
+            }
+        }
+
+        public void RemoveSugar(int number)
+        {
+            sugar -= number;
+
+            if(sugar < 0)
+            {
+                sugar = 0;
+            }
+        }
+
+        public void RemoveIce(int number)
+        {
+            ice -= number;
+
+            if(ice < 0)
+            {
+                ice = 0;
+            }
+        }
+    }
+}
