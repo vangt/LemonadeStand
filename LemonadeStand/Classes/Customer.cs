52ddffd2a69e1bc9e89d6ad8c9a22877f9d24bc5
vangt <vangt86@gmail.com>
2017-08-10 09:13:37 -0500

    Customers now have a thirst method.

---
+++ LemonadeStand/Classes/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LemonadeStand.Classes
+{
+    public class Customer
+    {
+        private double thirst;
+        
+        public Customer()
+        {
+            thirst = GetThirst;
+        }
+
+        public double GetThirst
+        {
+            get
+            {
+                return thirst;
+            }
+            set
+            {
+                Random random = new Random();
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                Thread.Sleep(1000);
+                stopWatch.Stop();
+                thirst = random.Next(40, 61);
+            }
+        }
+    }
+}
