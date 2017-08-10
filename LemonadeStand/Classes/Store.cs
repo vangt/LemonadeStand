42ca13a3ec21983b1afa84f1972c88202b49b48a
vangt <vangt86@gmail.com>
2017-08-10 15:12:52 -0500

    Added store and methods to get prices.

---
+++ LemonadeStand/Classes/Store.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand.Classes
+{
+    public class Store
+    {
+        Lemon lemon = new Lemon();
+        Sugar sugar = new Sugar();
+        Ice ice = new Ice();
+        Cup cup = new Cup();
+
+        public Store()
+        {
+
+        }
+
+        public double GetLemonPrice
+        {
+            get
+            {
+                return lemon.Price;
+            }
+        }
+
+        public double GetSugarPrice
+        {
+            get
+            {
+                return sugar.Price;
+            }
+        }
+
+        public double GetIcePrice
+        {
+            get
+            {
+                return ice.Price;
+            }
+        }
+
+        public double GetCupPrice
+        {
+            get
+            {
+                return cup.Price;
+            }
+        }
+    }
+}
