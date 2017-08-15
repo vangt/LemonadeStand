1f87a43799f042c0339c854b2d67705fcb6ebd01
vangt <vangt86@gmail.com>
2017-08-15 15:29:06 -0500

    Testing Database code.

---
+++ LemonadeStand/DatabaseLoader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public class DatabaseLoader
+    {
+        SqlConnection connection;
+        public DatabaseLoader()
+        {
+            connection = new SqlConnection("Server=TSHWMSIM;Database=LemonadeStand;Integrated Security=true");
+        }
+
+        public void LoadGame()
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT Player_Name, Player_Net FROM Scores", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine("Player: {0} \nTotal Net: {1}", reader.GetString(0), reader.GetString(1));
+                    Console.WriteLine("+++++++++++++++++");
+                }
+                reader.Close();
+                connection.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LemonadeStand/DatabaseSaver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    public class DatabaseSaver
+    {
+        SqlConnection connection;
+        public DatabaseSaver()
+        {
+            connection = new SqlConnection("Server=TSHWMSIM;Database=LemonadeStand;Integrated Security=true");
+        }
+
+        public void Save(string name, double net)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand($"INSERT INTO Scores VALUES ('{playerName}', '{playerNet}')", connection);
+                command.ExecuteNonQuery();
+                connection.Close();
+                Console.WriteLine("Game Saved");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
